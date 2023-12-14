4c695882c8261c12294a8d1c009a59b2546dd71c
jogebro <joelabril2110@gmail.com>
2023-12-14 14:19:16 -0500

    feat: :sparkles: Ejercicio 7

--- Ejercicio-1.js
+++ Ejercicio-1.js
@@ -1,13 +1,12 @@
 /* Hallar el promedio de notas de un curos de N estudiantes */
 
-let estudiantes = 2;
+const estudiantes = parseInt(prompt("Numero de estudiantes"));
 let notas = 0;
 
 for (let i = 1; i <= estudiantes; i++) {
     const nota = parseInt(prompt(`Nota del estudiante ${[i]}`));
     notas = notas + nota;
-    alert(notas);
 }
 
 const promedio = notas / estudiantes;
-alert(promedio);
+alert("Promedio de notas: " + promedio);
--- Ejercicio-7.js
+++ Ejercicio-7.js
@@ -10,21 +10,37 @@ class Producto{
         this.porcentajeDescuento = porcentajeDescuento
     }
 
-    productoExiste(){
-        return this.descripcion;
+    soliProduct(nombre){
+        if (nombre == this.descripcion) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
-let productos = [
-    {
-        codigo: 1,
-        descripcion: "leche",
-        precioCompra: 2000,
-        cantidadMinBodega: 10,
-        cantidadMaxBodega: 100,
-        porcentajeDescuento: 5
-    }
-]
+const op = -1;
 
-const producto = new Producto (productos[0]);
-console.log(producto.productoExiste());
+while(op !== 0){
+    const options = parseInt(prompt(`
+        1). Solicitar Pedido
+        0). CERRAR
+    `))
+
+    switch (options) {
+        case 1:
+            const acetaminofen = new Producto ("A1", "acetaminofen", 2000, 0, 100, 2);
+            const busqueda = prompt("Producto a buscar: ").toLowerCase();
+            alert(acetaminofen.soliProduct(busqueda));
+            break;
+    
+        case 0:
+            op = 0;
+            break;
+
+        default:
+            alert("Opcion no valida");
+            break;
+    }
+}
+
